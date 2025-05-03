3d25f2f2632c5935f8e4da821e7b06b9ba5d26af
Arunachalam_RM <Arunachalam_RM@outlook.com>
2025-05-03 23:42:35 +0530

    Add project files.

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassLibrary
+{
+    public class Student
+    {
+        private int grade = 2;
+
+        public int GetGrade()
+        {
+            return grade;
+        }
+        public ref int GetGradeRef()
+        {
+            return ref grade;
+        }
+
+        public ref int GetArrayIndexRef(int[] arr,int index)
+        {
+            return ref arr[index];
+        }
+
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary;
+
+namespace Main
+{
+    class Sample
+    {
+        public static void Main(string[] args)
+        {
+            Student bob = new Student();
+            System.Console.WriteLine("before modification:" + bob.GetGrade());//2
+            ref int g=ref bob.GetGradeRef();
+            g = 10;
+            System.Console.WriteLine("after modification:" + bob.GetGrade());//10
+
+
+            int[] arr = { 10, 65, 65, 26, 87 };
+            System.Console.WriteLine("before modification:"+arr[2]);//65
+            ref int value = ref bob.GetArrayIndexRef(arr, 2);
+            value = 76;
+            System.Console.WriteLine("after modification:" + arr[2]);//76
+        }
+    }
+}
